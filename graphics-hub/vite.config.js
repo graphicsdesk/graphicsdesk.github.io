085ae8e4e884de908b394d647138e57f70cccd9d
Jenny Ye <yejenny2004@gmail.com>
2025-08-27 14:37:44 -0400

    editing path

--- graphics-hub/vite.config.js
+++ graphics-hub/vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/graphics-hub/',
+  base: '/',
   plugins: [svelte()],
 })
